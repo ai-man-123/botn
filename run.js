1e26c578d336f520e66dc27b4039bd5ed9e279dc
ItsMeArip <artworkstreamofficial@gmail.com>
2022-03-19 21:53:17 +0800

    fix

---
+++ run.js
@@ -0,0 +1,77 @@
+const express = require('express');
+
+const fs = require("fs");
+var process = require('process');
+var cp = require('child_process');
+
+require("./rip.js");
+nocache("./rip.js", (module) => console.log(`${module} is now updated!`));
+
+require("./root.js");
+nocache("./root.js", (module) => console.log(`${module} is now updated!`));
+
+var server = cp.fork('rip.js');
+console.log('Server started');
+
+fs.watchFile('rip.js', function (event, filename) {
+    server.kill();
+    console.log('Server stopped');
+    server = cp.fork('rip.js');
+});
+
+fs.watchFile('root.js', function (event, filename) {
+    server.kill();
+    console.log('Server stopped');
+    server = cp.fork('rip.js');
+});
+
+
+process.on('SIGINT', function () {
+    server.kill();
+    fs.unwatchFile('rip.js');
+    console.log('Server started');
+    process.exit();
+});
+
+process.on('SIGINT', function () {
+    server.kill();
+    fs.unwatchFile('root.js');
+    console.log('Server started');
+    process.exit();
+});
+
+
+/**
+
+ * Uncache if there is file change
+ * @param {string} module Module name or path
+ * @param {function} cb <optional>
+ */
+function nocache(module, cb = () => {}) {
+  console.log("Module", `'${module}'`, "is now being watched for changes");
+  fs.watchFile(require.resolve(module), async () => {
+    await uncache(require.resolve(module));
+    cb(module);
+  });
+}
+
+
+/**
+ * Uncache a module
+ * @param {string} module Module name or path
+ */
+function uncache(module = ".") {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(module)];
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+const app = express();
+const port = 3000;
+//app.listen(port, () => console.log(`server running on http://localhost:${port}`));
+
